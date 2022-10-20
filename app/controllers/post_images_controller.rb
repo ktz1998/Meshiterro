dea398cdd55deee7ec5afd8626e1be68290dffab
ktz1998 <doraemonfan.kosuke@gmail.com>
2022-10-20 08:20:54 +0000

    End 24章

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -14,19 +14,19 @@ class PostImagesController < ApplicationController
  end
 
   def index
-    @post_images = PostImage.all
+    @post_images = PostImage.page(params[:page])
   end
 
   def show
     @post_image = PostImage.find(params[:id])
     @post_comment = PostComment.new
   end
-  
+
   def destroy
     @post_image = PostImage.find(params[:id])
     @post_image.destroy
     redirect_to '/post_images'
-  end 
+  end
 
   private
 
